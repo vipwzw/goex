64f14bb1eebd0060e412f1e7d0c302a76150002e
vipwzw <vipwzw@vip.qq.com>
2020-03-10 16:26:33 +0800

    add huobi example

---
+++ example/huobi.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/nntaoli-project/goex"
+	"github.com/nntaoli-project/goex/huobi"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:wzw123456@/trade")
+	if err != nil {
+		panic(err)
+	}
+	ws := huobi.NewHbdmWs() //huobi期货
+	//设置回调函数
+	ws.SetCallbacks(func(ticker *goex.FutureTicker) {
+		//printJSON(ticker)
+	}, func(depth *goex.Depth) {
+		log.Println(depth)
+	}, func(trade *goex.Trade, contract string) {
+		saveToDB(db, trade)
+		if trade.Amount > 200 {
+			log.Println(trade)
+		}
+	})
+	//订阅行情
+	ws.SubscribeTrade(goex.BTC_USDT, goex.QUARTER_CONTRACT)
+	//ws.SubscribeDepth(goex.BTC_USDT, goex.QUARTER_CONTRACT, 5)
+	//ws.SubscribeTicker(goex.BTC_USDT, goex.QUARTER_CONTRACT)
+	select {}
+}
+
+func printJSON(v interface{}) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(data))
+}
+
+func saveToDB(db *sql.DB, trade *goex.Trade) {
+	q := fmt.Sprintf(
+		"insert into trade.huobi_quarter_btcusdt(tid, type, amount, price, time_ms) values ('%d', '%d', '%f', '%f', '%d')", trade.Tid, trade.Type, trade.Amount, trade.Price, trade.Date)
+	tryInsert(db, q)
+}
+
+func tryInsert(db *sql.DB, q string) {
+	_, err := db.Exec(q)
+	if err != nil {
+		log.Println("exec failed:", err, ", sql:", q)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Kucoin/kucoin-go-sdk v1.2.2
 	github.com/deckarep/golang-set v1.7.1
 	github.com/go-openapi/errors v0.19.2
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/websocket v1.4.1
 	github.com/json-iterator/go v1.1.7
